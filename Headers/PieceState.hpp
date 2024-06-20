18f9e2caa8fa6aac734352d2a1f2fd405487a303
nathandavis18 <nathandavis8@mail.weber.edu>
2024-06-20 12:45:54 -0600

    Piece and State structs no longer static

--- Headers/PieceState.hpp
+++ Headers/PieceState.hpp
@@ -9,7 +9,7 @@ public:
 	/// <summary>
 	/// The Piece information, including the piece data and the width of the piece
 	/// </summary>
-	static struct Piece {
+	struct Piece {
 		Piece() : width(0), data(0) {}
 		Piece(const std::vector<u8> data, const u8 width) : width(width), data(data) {}
 
@@ -20,7 +20,7 @@ public:
 	/// <summary>
 	/// The state of the piece. Which rotation it is in, how far left/right it has moved, and how far down it has moved.
 	/// </summary>
-	static struct State {
+	struct State {
 		std::unique_ptr<Piece> piece;
 		std::unique_ptr<Piece> nextPiece;
 		std::unique_ptr<Piece> heldPiece;
